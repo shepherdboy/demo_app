3316ca88a02292f112f45610d12efb85c84d4505
faithpst <faithpst@163.com>
2013-09-04 00:51:13 +0800

    Finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 DemoApp::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :microposts
+
+  resources :users
+
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
