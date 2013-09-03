632968c5330255a4a6aa8bcff8f939d336c78597
faithpst <faithpst@163.com>
2013-09-03 22:57:45 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'e685704d1626bc5fba9bc68fcaaf061a0d0e33e82c383536bbc6540fa455a7cfa70b1ceece4b8307678e18417473a0bbbd20751b1a84c91530bb0996d9422f67'
